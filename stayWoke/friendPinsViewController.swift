92ac492760a15146b6be78efa6e880ad426b327d
Katie De la Paz <kdelapaz583@gmail.com>
2020-07-15 10:15:01 -0500

    Katie: I tidied up the segues and changed some design colors. Also, I added files for the Your Pins and Friend Pins view controllers.

---
+++ stayWoke/friendPinsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  friendPinsViewController.swift
+//  stayWoke
+//
+//  Created by Katie De la Paz on 7/14/20.
+//  Copyright © 2020 Claire Ruttencutter. All rights reserved.
+//
+
+import UIKit
+
+class friendPinsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
