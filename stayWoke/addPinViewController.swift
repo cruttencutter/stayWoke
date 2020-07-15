1ba581f0f7f41530ce1e5e1122c24c0a781428b7
cruttencutter <20cruttencutter@nerinxhs.org>
2020-07-15 17:55:49 -0500

    Finished the Petition and Graphics pages and am currently working on a new view controller where the user can choose to save pins

---
+++ stayWoke/addPinViewController.swift
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//  addPinViewController.swift
+//  stayWoke
+//
+//  Created by Claire Ruttencutter on 7/15/20.
+//  Copyright © 2020 Claire Ruttencutter. All rights reserved.
+//
+
+import UIKit
+
+class addPinViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let colors = ["Red","Yellow","Green","Blue"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return colors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return colors[row]
+    }
+    
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        pickerView.delegate = self
+        pickerView.dataSource = self
+    }
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
