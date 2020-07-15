8628c8a68319efa5f9328d46542217745cc35e1f
Katie De la Paz <kdelapaz583@gmail.com>
2020-07-15 10:41:39 -0500

    Katie: I added the basic layout to the newsArticles and videos view controllers

---
+++ stayWoke/petitionsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  petitionsViewController.swift
+//  stayWoke
+//
+//  Created by Katie De la Paz on 7/15/20.
+//  Copyright © 2020 Claire Ruttencutter. All rights reserved.
+//
+
+import UIKit
+
+class petitionsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
