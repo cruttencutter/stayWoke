9640b51dd96325925290f1c962b6c7c87c275735
cruttencutter <20cruttencutter@nerinxhs.org>
2020-07-16 16:55:23 -0500

    Finished the rest of the links!

--- stayWoke/petitionsViewController.swift
+++ stayWoke/petitionsViewController.swift
@@ -10,21 +10,70 @@ import UIKit
 
 class petitionsViewController: UIViewController {
 
+    @IBOutlet weak var georgeFloydLink: UITextView!
+    @IBOutlet weak var breonnaTaylorLink: UITextView!
+    @IBOutlet weak var officersLInk: UITextView!
+    @IBOutlet weak var tonyMcdadeLink: UITextView!
+    @IBOutlet weak var ahmaudArberyLink: UITextView!
+    @IBOutlet weak var handsUpLink: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //George Floyd
+        let george = "https://www.change.org/p/mayor-jacob-frey-justice-for-george-floyd?recruiter=895898514&utm_source=share_petition&utm_medium=twitter&utm_campaign=psf_combo_share_initial&utm_term=psf_combo_share_initial&recruited_by_id=6a4669c0-a74d-11e8-a554-dd9015f4e479"
+        let attributedString = NSMutableAttributedString(string: "Change.org")
+        let text = "Change.org"
+        let str = NSString(string: text)
+        let theRange = str.range(of: "Change.org")
+        attributedString.addAttribute(.link, value: george, range: theRange)
+        georgeFloydLink.attributedText = attributedString
+        
+        //Breonna Taylor
+        let breonna = "https://www.change.org/p/andy-beshear-justice-for-breonna-taylor"
+        let attributedString1 = NSMutableAttributedString(string: "Change.org")
+        let text1 = "Change.org"
+        let str1 = NSString(string: text1)
+        let theRange1 = str1.range(of: "Change.org")
+        attributedString1.addAttribute(.link, value: breonna, range: theRange1)
+        breonnaTaylorLink.attributedText = attributedString1
+        
+        //Get the Officers Charged
+        let officers = "https://t.co/lPsRQq6GqD"
+        let attributedString2 = NSMutableAttributedString(string: "Change.org")
+        let text2 = "Change.org"
+        let str2 = NSString(string: text2)
+        let theRange2 = str2.range(of: "Change.org")
+        attributedString2.addAttribute(.link, value: officers, range: theRange2)
+        officersLInk.attributedText = attributedString2
+        
+        //Tony McDade
+        let tony = "https://www.change.org/p/black-lives-matter-activists-justice-for-tony-mcdade"
+        let attributedString3 = NSMutableAttributedString(string: "Change.org")
+        let text3 = "Change.org"
+        let str3 = NSString(string: text3)
+        let theRange3 = str3.range(of: "Change.org")
+        attributedString3.addAttribute(.link, value: tony, range: theRange3)
+        tonyMcdadeLink.attributedText = attributedString3
+        
+        //Ahmaud Arbery
+        let ahmaud = "https://www.change.org/p/federal-bureau-of-investigation-disbarment-of-george-e-barnhill"
+        let attributedString4 = NSMutableAttributedString(string: "Change.org")
+        let text4 = "Change.org"
+        let str4 = NSString(string: text4)
+        let theRange4 = str4.range(of: "Change.org")
+        attributedString4.addAttribute(.link, value: ahmaud, range: theRange4)
+        ahmaudArberyLink.attributedText = attributedString4
+        
+        //Hands Up Act
+          let hands = "https://www.change.org/p/us-senate-hands-up-act"
+          let attributedString5 = NSMutableAttributedString(string: "Change.org")
+          let text5 = "Change.org"
+          let str5 = NSString(string: text5)
+          let theRange5 = str5.range(of: "Change.org")
+          attributedString5.addAttribute(.link, value: hands, range: theRange5)
+          handsUpLink.attributedText = attributedString5
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- stayWoke/videosViewController.swift
+++ stayWoke/videosViewController.swift
@@ -9,22 +9,72 @@
 import UIKit
 
 class videosViewController: UIViewController {
-
+    
+    @IBOutlet weak var unusAnnusLink: UITextView!
+    @IBOutlet weak var tedxTalksLink: UITextView!
+    @IBOutlet weak var stiffLaurenLink: UITextView!
+    @IBOutlet weak var nbcNewsLink: UITextView!
+    @IBOutlet weak var jadaJonesLink: UITextView!
+    @IBOutlet weak var globalCitizenLInk: UITextView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        //Unus Annus
+        let unus = "https://www.youtube.com/watch?v=xNB-CVDuITA"
+        let attributedString6 = NSMutableAttributedString(string: "Unus Annus")
+        let text6 = "Unus Annus"
+        let str6 = NSString(string: text6)
+        let theRange6 = str6.range(of: "Unus Annus")
+        attributedString6.addAttribute(.link, value: unus, range: theRange6)
+        unusAnnusLink.attributedText = attributedString6
+        
+        //Tedx Talks
+        let ted = "https://www.youtube.com/watch?v=Sd-VUOgS3rE"
+        let attributedString1 = NSMutableAttributedString(string: "TEDx Talks")
+        let text1 = "TEDx Talks"
+        let str1 = NSString(string: text1)
+        let theRange1 = str1.range(of: "TEDx Talks")
+        attributedString1.addAttribute(.link, value: ted, range: theRange1)
+        tedxTalksLink.attributedText = attributedString1
+        
+        //Stiff Lauren
+        let stiff = "https://www.youtube.com/watch?v=0Em5cZFocQ0&list=PLUarU1HDBmly4K3_6hfNpvH2V97IaODXn"
+        let attributedString2 = NSMutableAttributedString(string: "Stiff Lauren")
+        let text2 = "Stiff Lauren"
+        let str2 = NSString(string: text2)
+        let theRange2 = str2.range(of: "Stiff Lauren")
+        attributedString2.addAttribute(.link, value: stiff, range: theRange2)
+        stiffLaurenLink.attributedText = attributedString2
+        
+        //NBC News
+        let nbc = "https://www.youtube.com/watch?v=mI7eHX9u4Q0"
+        let attributedString3 = NSMutableAttributedString(string: "NBC News")
+        let text3 = "NBC News"
+        let str3 = NSString(string: text3)
+        let theRange3 = str3.range(of: "NBC News")
+        attributedString3.addAttribute(.link, value: nbc, range: theRange3)
+        nbcNewsLink.attributedText = attributedString3
+        
+        //Jada Jones
+        let jada = "https://www.youtube.com/watch?v=NKGkXoF60Ik"
+        let attributedString4 = NSMutableAttributedString(string: "Jada Jones")
+        let text4 = "Jada Jones"
+        let str4 = NSString(string: text4)
+        let theRange4 = str4.range(of: "Jada Jones")
+        attributedString4.addAttribute(.link, value: jada, range: theRange4)
+        jadaJonesLink.attributedText = attributedString4
+        
+        //Global Citizen
+          let global = "https://www.youtube.com/watch?v=c1IopsKY0dY"
+          let attributedString5 = NSMutableAttributedString(string: "Global Citizen")
+          let text5 = "Global Citizen"
+          let str5 = NSString(string: text5)
+          let theRange5 = str5.range(of: "Global Citizen")
+          attributedString5.addAttribute(.link, value: global, range: theRange5)
+          globalCitizenLInk.attributedText = attributedString5
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
